013f890cc3916c69342be781bf39258e142f3fe5
Lapprenti <contact@lapprand.pro>
2024-07-30 17:37:15 +0200

    Initial minimul viable script

---
+++ README.md
@@ -0,0 +1,75 @@
+# JO2024 ICS Builder
+
+This script allows you to create an ICS file containing the schedule of events for the 2024 Olympic Games in Paris, France. The ICS file can be imported into a calendar application to view the schedule of events.
+
+## Illustration
+
+![Example Screenshot](docs/screenshot.png)
+
+## Usage
+
+To use the script, you'll need to have Python 3 installed on your system. You can then install the required dependencies by running the following command:
+
+```sh
+pip install -r requirements.txt
+```
+
+Once you have the required dependencies installed, you can run the script by executing the following command:
+
+```sh
+python jo2024_ics_builder.py
+```
+
+By default, the script will create an ICS file containing the schedule of events for July 30, 2024. You can specify a different date or date range by modifying the `start_date` and `end_date` variables at the bottom of the script.
+
+The ICS file will be saved in the `output` folder, with a filename that includes the start and end dates of the date range you specified. For example, if you specify a date range of July 30 to August 10, 2024, the filename will be `JO2024-07-30-to-08-10.ics`.
+
+After having the file you will then be able to import the events into your favorite calendar app.
+
+## Data Source
+
+The script retrieves the schedule of events from the following API endpoint:
+
+```bash
+https://sph-s-api.olympics.com/summer/schedules/api/{lang}/schedule/day/{date_str}
+``` 
+
+where `{date_str}` is the date for which you want to retrieve the schedule of events, in the format `YYYY-MM-DD`.
+
+## Filters
+
+The script allows you to filter the events based on nations and sports. You can specify a list of nations and sports that you are interested in. The script will include only the events that match the specified filters.
+
+### Example
+
+```python
+lang = 'FRA'  # 'ENG' <-- If you want events in English (final ICS)
+start_date = datetime(2024, 7, 30)
+end_date = datetime(2024, 8, 10)  # Optional - if not set, will match the start date (entire day)
+nations = ['FRA']  # Optional
+sports = ['Badminton', 'Tennis', 'BMX', 'Skate', 'Gym', 'Table Tennis', 'Judo', 'Fencing']  # Optional
+
+# Main entry point
+create_ics(lang=lang, start_date=start_date, end_date=end_date, nations=nations, sports=sports)
+```
+
+> NB : Sports are processed with the values provided. Match in lower case from `/assets/sports-codes.json` full name. Then the process is filtering on sports code.
+
+## Dependencies
+
+The script requires the following Python packages:
+
+    ics: A library for working with iCalendar (ICS) files.
+    requests: A library for making HTTP requests.
+
+
+## License
+
+This script is licensed under the MIT License. See the LICENSE file for more information.
+Contributing
+
+Contributions are welcome! Please open an issue or submit a pull request if you have any suggestions or improvements.
+
+## Acknowledgments
+
+This script was created to help users easily generate ICS files for the 2024 Olympic Games schedule.
---
+++ ics-builder.py
@@ -0,0 +1,120 @@
+from ics import Calendar, Event
+from datetime import datetime, timedelta
+import requests
+import json
+import os
+
+def create_event_with_unit(unit):
+    # Create a new event
+    e = Event()
+
+    # Set the event's name
+    e.name = f"{unit['disciplineName']}: {unit['eventUnitName']} - {unit['phaseName']}"
+
+    # Set the event's start and end times
+    e.begin = datetime.fromisoformat(unit['startDate'].replace('Z', '+00:00'))
+    e.end = datetime.fromisoformat(unit['endDate'].replace('Z', '+00:00'))
+
+    # Set the event's location
+    e.location = f"{unit['venueDescription']}, {unit['locationDescription']}"
+
+    # Set the event's description
+    e.description = f"Status: {unit['statusDescription']}\n"
+    for competitor in unit['competitors']:
+        if 'noc' in competitor:
+            e.description += f"{competitor['name']} ({competitor['noc']})\n"
+        else:
+            e.description += f"{competitor['name']}\n"
+
+    return e
+
+def create_ics(lang, start_date, end_date=None, nations=None, sports=None):
+    # If no end date is provided, set it to the start date
+    if end_date is None:
+        end_date = start_date
+
+    if nations is not None:
+        print('Nations')
+        print(nations)
+
+    discipline_codes = []
+    if sports is not None:
+        # Load the sports codes from the JSON file
+        with open(os.path.join(os.path.dirname(__file__), 'assets', 'sports-codes.json')) as f:
+            sports_codes = json.load(f)
+
+        # Find the discipline codes that correspond to the sports list
+        for sport in sports:
+            for code, name in sports_codes.items():
+                if sport.lower() in name.lower():
+                    discipline_codes.append(code)
+
+        # Remove any duplicate discipline codes
+        discipline_codes = list(set(discipline_codes))
+        print("Sports codes found :")
+        print(discipline_codes)
+
+    # Create a new calendar
+    c = Calendar()
+
+    # Iterate over each date in the range
+    current_date = start_date
+    while current_date <= end_date:
+        # Format the date as a string
+        date_str = current_date.strftime('%Y-%m-%d')
+
+        # Construct the URL for the data
+        url = f'https://sph-s-api.olympics.com/summer/schedules/api/{lang}/schedule/day/{date_str}'
+        print(url)
+
+        try:
+            # Send a GET request to the URL
+            headers = {'User-Agent': 'Mozilla/5.0'}
+            response = requests.get(url, headers=headers)
+        except requests.exceptions.RequestException as e:
+            print(f"Error: {e}")
+
+        # If the request was successful, process the data
+        if response.status_code == 200:
+            data = response.json()
+
+            # Iterate over each unit in the data
+            for unit in data['units']:
+                # Check if the unit matches the filters
+                if nations is None and sports is None:
+                    print("NO FILTER : " + f"{unit['disciplineName']}: {unit['eventUnitName']} - {unit['phaseName']}")
+                    c.events.add(create_event_with_unit(unit))
+                    continue
+                
+                if nations is not None and any('noc' in competitor and competitor['noc'] in nations for competitor in unit['competitors']):
+                    print("NATION : " + f"{unit['disciplineName']}: {unit['eventUnitName']} - {unit['phaseName']}")
+                    c.events.add(create_event_with_unit(unit))
+                    continue
+
+                if discipline_codes and unit['disciplineCode'] in discipline_codes:
+                    print("SPORT : " + f"{unit['disciplineName']}: {unit['eventUnitName']} - {unit['phaseName']}")
+                    c.events.add(create_event_with_unit(unit))
+                    continue
+
+        # Move to the next date
+        current_date += timedelta(days=1)
+
+    # Write the calendar to a file
+    output_folder = 'output'
+    os.makedirs(output_folder, exist_ok=True)
+    start_str = start_date.strftime('%Y-%m-%d')
+    end_str = end_date.strftime('%Y-%m-%d')
+    filename = f'JO2024-{start_str}-to-{end_str}.ics'
+    filepath = os.path.join(output_folder, filename)
+    with open(filepath, 'w') as ics_file:
+        ics_file.writelines(c)
+
+# Variables definition
+lang = 'FRA'  # 'ENG' <-- If you want events in English (final ICS)
+start_date = datetime(2024, 7, 30)
+end_date = datetime(2024, 8, 10)  # Optional - if not set, will match the start date (entire day)
+nations = ['FRA']  # Optional
+sports = ['Badminton', 'Tennis', 'BMX', 'Skate', 'Gym', 'Table Tennis', 'Judo', 'Fencing']  # Optional
+
+# Main entry point
+create_ics(lang=lang, start_date=start_date, end_date=None, nations=None, sports=None)
